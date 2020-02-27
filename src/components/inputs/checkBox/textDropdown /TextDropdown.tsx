080eacae7125bc6f95863954b866d1b7481af0b6
Vahid Davoudi <vahid@aleph-labs.com>
2020-02-27 09:57:05 +0800

    dropdown styled

--- src/components/inputs/checkBox/textDropdown /TextDropdown.tsx
+++ src/components/inputs/checkBox/textDropdown /TextDropdown.tsx
@@ -9,6 +9,61 @@ interface Props {
 }
 interface State {}
 
+const customStyles = {
+  control: (provided: any) => ({
+    ...provided,
+    border: "hidden",
+    backgroundColor: "transparent",
+
+    "&:hover": {
+      borderColor: "red"
+    },
+    boxShadow: "none"
+  }),
+
+  menu: (provided: any) => ({
+    ...provided,
+    backgroundColor: "transparent",
+    boxShadow: "none",
+    border: "none",
+    display: "block"
+  }),
+
+  menuList: (base: any) => ({
+    ...base,
+    backgroundColor: "transparent",
+    // kill the white space on first and last option
+    padding: 10
+  }),
+
+  option: (styles: any, { data, isDisabled, isFocused, isSelected }: any) => {
+    return {
+      ...styles,
+      backgroundColor: isDisabled
+        ? null
+        : isSelected
+        ? data.color
+        : isFocused
+        ? "transparent"
+        : null,
+      color: isDisabled
+        ? "#ccc"
+        : isSelected
+        ? "black"
+          ? "black"
+          : "black"
+        : "black",
+      cursor: isDisabled ? "not-allowed" : "default",
+
+      ":active": {
+        ...styles[":active"],
+        backgroundColor:
+          !isDisabled && (isSelected ? data.color : "transparent")
+      }
+    };
+  }
+};
+
 class TextDropdown extends Component<Props, State> {
   render() {
     const { options, selectedOption, handleChange, placeHolder } = this.props;
@@ -19,6 +74,7 @@ class TextDropdown extends Component<Props, State> {
           value={selectedOption}
           onChange={handleChange}
           options={options}
+          styles={customStyles}
         />
       </div>
     );
