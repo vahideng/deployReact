18a86398f1ef4f946f0d6bbdc60073e6f3fab7cd
Edward Aldrin Castro <aldrin@aleph-labs.com>
2020-03-09 11:41:45 +0800

    [BANNER-HERO] updated component

--- src/components/banners/bannerHero/BannerHero.tsx
+++ src/components/banners/bannerHero/BannerHero.tsx
@@ -37,25 +37,26 @@ const BannerHero: React.FC<Props> = ({
     <div id={`${testId}`}>
       <Row>
         <Col xs={12} sm={6} style={{ padding: "0" }}>
-          <img src={leftImage} style={{ width: "100%" }} />
+          <img className={classes.ImageContainer} src={leftImage} />
         </Col>
-        <Col xs={12} sm={5} className={classes.Container}>
+        <Col xs={12} sm={6} className={classes.Container}>
           <SB_15_BLACK style={{ textTransform: "uppercase" }}>
             {label}
           </SB_15_BLACK>
           <B_32_BLACK>{title}</B_32_BLACK>
-          <div style={{ paddingTop: "16px" }}>
+          <div style={{ paddingTop: "1rem" }}>
             <R_20_BLACK>{content}</R_20_BLACK>
           </div>
           {showButton && buttonClick && buttonText ? (
-            <div style={{ paddingTop: "72px" }}>
+            <div style={{ paddingTop: "4.5rem" }}>
               <Col xs={12} sm={6} className={classes.Button}>
                 <PrimaryButton
                   testId={`${testId}-0`}
                   buttonColor={!!buttonColor}
                   title={buttonText}
                   onButtonClick={buttonClick}
-                  width={"100%"}
+                  height="3.125rem"
+                  width="100%"
                 />
               </Col>
             </div>
