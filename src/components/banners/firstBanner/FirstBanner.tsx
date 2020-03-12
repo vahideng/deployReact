05094bd53d78df6261508a17efa3133a99b8621b
Edward Aldrin Castro <aldrin@aleph-labs.com>
2020-03-12 16:42:12 +0800

    [FIRST BANNER] FirstBanner component

---
+++ src/components/banners/firstBanner/FirstBanner.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+import classes from "./FirstBanner.module.css";
+
+interface Props {
+  content: string;
+  image: string;
+  onClick?: () => void;
+  tag: string;
+  testId?: string;
+  title: string;
+}
+
+const FirstBanner: React.FC<Props> = ({
+  content,
+  image,
+  tag,
+  testId,
+  title
+}: Props) => {
+  return (
+    <div className={classes.Container} id={`${testId}`}>
+      <div className={classes.Overlay}>
+        <img className={classes.ImageContainer} src={image} />
+        <div className={classes.Details}>
+          <div className={classes.Tag}>
+            <p className={classes.TagText}>{tag}</p>
+          </div>
+          <p className={classes.Title}>{title}</p>
+          <p className={classes.Content}>{content}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FirstBanner;
