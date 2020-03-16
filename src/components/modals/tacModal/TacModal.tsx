6d7a54e446203e860520f3161b9aaf1d0a7d5e89
Hossein Beigzadeh <hossein@aleph-labs.com>
2020-03-16 14:41:02 +0800

    Merged with TacModal updates for SP-1

--- src/components/modals/tacModal/TacModal.tsx
+++ src/components/modals/tacModal/TacModal.tsx
@@ -1,10 +1,10 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
 import Modal from "react-modal";
-
 import InputField from "../../inputs/inputFields/InputFields";
 import Paragraphs from "../../assets/typography";
 import FullButton from "../../buttons/primaryButton/PrimaryButton";
+import classes from "./TacModal.module.css";
+import Icon from "src/components/assets/icons/icon";
 
 const { R_15_GREY444 } = Paragraphs;
 
@@ -15,6 +15,12 @@ interface Props {
   handleChange?: any;
   label?: string;
   value?: string;
+  notValid?: boolean;
+  errorMessage?: {
+    testId?: string;
+    errorText: string;
+    subText: string;
+  };
   buttonTitle?: string;
   onButtonClick?: any;
   buttonColor?: any;
@@ -41,32 +47,33 @@ const TacModal: React.FC<Props> = ({
   handleChange,
   label,
   value,
+  notValid,
+  errorMessage,
   content,
   buttonTitle,
   onButtonClick,
   buttonColor,
   testId
 }) => {
+  const [modalStatus, setModalStatus] = useState(modalIsOpen);
+  useEffect(() => {
+    setModalStatus(modalIsOpen);
+  }, [modalIsOpen]);
   return (
-    <div>
-      <Modal
-        isOpen={!!modalIsOpen && modalIsOpen}
-        style={customStyles}
-        contentLabel="Example Modal"
-      >
-        <div className="row" id={testId}>
-          <div
-            style={{ display: "flex", justifyContent: "center", width: "80%" }}
-            className="col-lg-8"
-          >
+    <>
+      <Modal isOpen={!!modalStatus && modalStatus} style={customStyles}>
+        <div id={testId} className={classes.TacMainDiv} style={{}}>
+          <div id={testId} className={classes.TacMainInnerDiv} style={{}}>
             <form>
               <InputField
                 testId={`${testId}-0`}
                 handleChange={handleChange}
-                type="text"
+                type="password"
                 label={label}
                 icon={{ name: "TAC" }}
                 value={value}
+                notValid={notValid}
+                errorMessage={errorMessage}
               />
               <R_15_GREY444
                 style={{
@@ -78,21 +85,29 @@ const TacModal: React.FC<Props> = ({
                 {content}
               </R_15_GREY444>
             </form>
-          </div>
-          <div
-            style={{ alignSelf: "center", paddingBottom: "26px" }}
-            className="col-lg-4"
-          >
-            <FullButton
-              testId={`${testId}-1`}
-              buttonColor={{ top: buttonColor.top, bottom: buttonColor.bottom }}
-              title={buttonTitle}
-              onButtonClick={!!onButtonClick && onButtonClick}
-            />
+            <div className={classes.TacButton}>
+              <FullButton
+                testId={`${testId}-1`}
+                buttonColor={{
+                  top: buttonColor.top,
+                  bottom: buttonColor.bottom
+                }}
+                title={buttonTitle}
+                onButtonClick={!!onButtonClick && onButtonClick}
+              />
+            </div>
+            <span
+              className={classes.TacCrossButton}
+              onClick={() => {
+                setModalStatus(false);
+              }}
+            >
+              <Icon icon={"Fail-2"} size={15} />
+            </span>
           </div>
         </div>
       </Modal>
-    </div>
+    </>
   );
 };
 
