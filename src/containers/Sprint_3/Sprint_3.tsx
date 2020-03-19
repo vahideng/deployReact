2fb54a4ae6b53a9bdfe4cba361314c70700e2819
Edward Aldrin Castro <aldrin@aleph-labs.com>
2020-03-19 10:22:12 +0800

    added sprint 3 container

---
+++ src/containers/Sprint_3/Sprint_3.tsx
@@ -0,0 +1,222 @@
+import React, { useState } from "react";
+import { Redirect } from "react-router-dom";
+import Paragraphs from "../../components/assets/typography";
+import styled from "styled-components";
+import Navbar from "src/components/headers/navbar/Navbar";
+import NavbarTransparent from "src/components/headers/navbarTransparent/NavbarTransparent";
+import TextDropdown from "src/components/inputs/texDropdown/TextDropdown";
+import TextButton from "src/components/buttons/textButton/TextButton";
+import SettingModalCenter from "src/components/modals/settingModalCenter/SettingModalCenter";
+import FullButton from "src/components/buttons/fullButton/FullButton";
+import BottomModal from "src/components/modals/bottomModal/BottomModal";
+import SettingSummary from "src/components/infographic/settingSummary/SettingSummary";
+import StatusFormContainer from "src/components/wrappers/statusFormContainer/StatusFormContainer";
+import PrimaryButton from "src/components/buttons/primaryButton/PrimaryButton";
+
+const { B_13_ORANGE_463 } = Paragraphs;
+const Title = styled(B_13_ORANGE_463)`
+  text-align: center;
+  padding: 5px 0;
+  margin: 5px 0;
+  border-top: 1px solid #ffa463;
+  border-bottom: 1px solid #ffa463;
+`;
+interface Props {}
+
+const Sprint3: React.FC<Props> = () => {
+  const [selectedItem, setSelectedOption] = useState("Persian");
+  const [sprint1, setSprint1] = useState(false);
+  const [sprint2, setSprint2] = useState(false);
+  const [sprint3, setSprint3] = useState(false);
+  const [homeRedirect, setHomeRedirect] = useState(false);
+  const [settingModalCenter, setSettingModalCenter] = useState(false);
+  const [settingModal, setSettingModal] = useState(false);
+  if (sprint1 === true) {
+    return <Redirect to="/sprint-1" />;
+  }
+
+  if (sprint2 === true) {
+    return <Redirect to="/sprint-2" />;
+  }
+
+  if (sprint3 === true) {
+    return <Redirect to="/sprint-3" />;
+  }
+
+  if (homeRedirect === true) {
+    return <Redirect to="/" />;
+  }
+
+  return (
+    <div style={{ paddingTop: 100 }}>
+      <Navbar
+        icon={{
+          onIconClick: () => {
+            setHomeRedirect(true);
+          }
+        }}
+        profile={{
+          greeting: "Good Morning",
+          name: "Adam Constantine",
+          alt: "AVATAR",
+          src:
+            "https://images.unsplash.com/photo-1569913486515-b74bf7751574?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=935&q=80"
+        }}
+        rightButtons={[
+          {
+            iconName: "Time",
+            onButtonClick: () => alert("button-1-Clicked")
+          },
+          {
+            iconName: "Settings",
+            onButtonClick: () => alert("button-2-Clicked")
+          },
+          {
+            iconName: "Share",
+            onButtonClick: () => alert("button-3-Clicked")
+          }
+        ]}
+      />
+      <Title>BottomModal</Title>
+      <div style={{ padding: ".5rem 30%", background: "#f5f5f5" }}>
+        <FullButton
+          split
+          leftTitle="Open BottomModal"
+          rightTitle="Open SettingModalCenter"
+          onLeftButton={() => {
+            setSettingModal(!settingModal);
+          }}
+          onRightButton={() => {
+            setSettingModalCenter(!settingModalCenter);
+          }}
+        />
+      </div>
+      <BottomModal
+        testId={"testId"}
+        modalIsOpen={settingModal}
+        modalChildren={
+          <SettingSummary
+            icon={{ name: "Tick-1" }}
+            iconColor={{ top: "#94EC9B", bottom: "#5BB362" }}
+            detailTitle="Successfully Activated"
+            detail="Your account has been activated successfully."
+          />
+        }
+      />
+      <Title>SettingModalCenter</Title>
+      <SettingModalCenter
+        testId={"testId"}
+        modalIsOpen={settingModalCenter}
+        modalChildren={
+          <StatusFormContainer
+            statusIcon={{
+              icon: "Tick-1",
+              iconColor: { top: "#94EC9B", bottom: "#5BB362" },
+              outerIconColor: "#E5FCE6"
+            }}
+            children={
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
+                  padding: "2rem"
+                }}
+              >
+                <p style={{ textAlign: "center" }}>
+                  The amount entered exceeds the transaction limit set in your
+                  settings. Would you like to change your limit now?
+                </p>
+                <div
+                  style={{
+                    display: "flex",
+                    width: "100%",
+                    justifyContent: "space-between",
+                    padding: "1rem 0"
+                  }}
+                >
+                  <PrimaryButton
+                    title="No, thanks"
+                    onButtonClick={() => {
+                      alert("Button Clicked");
+                    }}
+                  />
+                  <PrimaryButton
+                    onButtonClick={() => {
+                      alert("Button Clicked");
+                    }}
+                    title="Change Limit"
+                    titleColor="#000000"
+                    buttonColor={{ top: "#F6F6F3", bottom: "#EAE9E3" }}
+                  />
+                </div>
+              </div>
+            }
+          />
+        }
+      />
+      <Title>Navbar Transparent</Title>
+      <NavbarTransparent
+        icon={{
+          onIconClick: () => {
+            setSprint1(true);
+          }
+        }}
+        rightButtons={[
+          <TextButton
+            buttonText="Sprint-1"
+            onTextClick={() => {
+              setSprint1(true);
+            }}
+            buttonStyles={{
+              color: "#000000",
+              fontWeight: 400,
+              fontSize: 15
+            }}
+          />,
+          <TextButton
+            buttonText="Sprint-2"
+            onTextClick={() => {
+              setSprint2(true);
+            }}
+            buttonStyles={{
+              color: "#000000",
+              fontWeight: 400,
+              fontSize: 15
+            }}
+          />,
+          <TextButton
+            buttonText="Sprint-3"
+            onTextClick={() => {
+              setSprint3(true);
+            }}
+            buttonStyles={{
+              color: "#000000",
+              fontWeight: 400,
+              fontSize: 15
+            }}
+          />,
+          <TextDropdown
+            // menuIsOpen
+            defaultValue={"Persian"}
+            handleChange={(selectedOption: any) => {
+              setSelectedOption(selectedOption);
+              console.log(`Option:`, selectedOption);
+            }}
+            selectedOption={selectedItem}
+            placeHolder={"English"}
+            options={[
+              { label: "English", value: "english" },
+              { label: "Malay", value: "malay" },
+              { label: "Persian", value: "persian" },
+              { label: "中文", value: "中文" }
+            ]}
+          />
+        ]}
+      />
+      Sprint 3
+    </div>
+  );
+};
+
+export default Sprint3;
