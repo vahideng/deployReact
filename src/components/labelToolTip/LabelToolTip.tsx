2dd8f73e19d3b86c2e962a82da30edec1b62fb5d
4twntybo <4twntybo@users.noreply.github.com>
2020-03-23 14:49:10 +0800

    label tooltip component

---
+++ src/components/labelToolTip/LabelToolTip.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Tooltip from "src/components/tooltip/Tooltip";
+import classes from "./LabelToolTip.module.css";
+//
+interface Props {
+  tooltipData: {
+    testId?: string;
+    tipChildren?: any;
+    color?: string;
+    spaceBetween?: any;
+  };
+  label?: string;
+}
+
+const LabelToolTip: React.FC<Props> = ({ tooltipData, label }) => {
+  return (
+    <div className={classes.toolTipContainer}>
+      <div className={classes.toolTipLabel}>{label}</div>
+      <div
+        className={classes.toolTipData}
+        style={{ paddingLeft: `${tooltipData.spaceBetween}` }}
+      >
+        <Tooltip
+          testId={tooltipData.testId}
+          tipChildren={tooltipData.tipChildren}
+          color={tooltipData.color}
+        />
+      </div>
+    </div>
+  );
+};
+export default LabelToolTip;
