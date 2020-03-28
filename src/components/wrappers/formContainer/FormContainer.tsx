a1abac1d68bfe16530091dbbeab98236bb5518c1
Edward Aldrin Castro <aldrin@aleph-labs.com>
2020-03-28 17:11:52 +0800

    updated existing FormContainer component

--- src/components/wrappers/formContainer/FormContainer.tsx
+++ src/components/wrappers/formContainer/FormContainer.tsx
@@ -1,10 +1,12 @@
-import React, { ReactNode } from "react";
+import React, { ReactNode, CSSProperties } from "react";
 import classes from "./FormContainer.module.css";
 import Paragraphs from "../../assets/typography";
 import Tooltip from "src/components/tooltip/Tooltip";
 import Icon from "src/components/assets/icons/icon";
 import StatusIcon from "src/components/assets/icons/statusIcon/StatusIcon";
-const { B_14_WHITE } = Paragraphs;
+import { Row } from "react-bootstrap";
+const { B_14_WHITE, B_15_ORANGE_463 } = Paragraphs;
+
 interface Props {
   testId?: string;
   statusIcon?: {
@@ -17,6 +19,11 @@ interface Props {
     image?: any;
   };
   label?: string;
+  rightLabel?: {
+    onClick?: () => void;
+    style?: CSSProperties;
+    label: string;
+  };
   tooltip?: ReactNode;
   children?: ReactNode;
 }
@@ -24,10 +31,16 @@ interface Props {
 const FormContainer: React.FC<Props> = ({
   children,
   label,
+  rightLabel,
   statusIcon,
   tooltip,
   testId
 }) => {
+  const rightLabelPointer: CSSProperties =
+    rightLabel !== undefined && rightLabel.onClick !== undefined
+      ? { cursor: "pointer" }
+      : {};
+
   return (
     <div>
       {!!statusIcon && (
@@ -70,15 +83,25 @@ const FormContainer: React.FC<Props> = ({
       >
         {!!label && (
           <div className={classes.FormContainerLabel} id={`${testId}-0`}>
-            <B_14_WHITE>{label}</B_14_WHITE>
-            {!!tooltip && (
-              <span className={classes.FormContainerTooltip}>
-                <Tooltip
-                  tipChildren={tooltip}
-                  color="#FFFFFF"
-                  testId={`${testId}-1`}
-                />
-              </span>
+            <Row className={classes.LeftLabel}>
+              <B_14_WHITE>{label}</B_14_WHITE>
+              {!!tooltip && (
+                <span className={classes.FormContainerTooltip}>
+                  <Tooltip
+                    tipChildren={tooltip}
+                    color="#FFFFFF"
+                    testId={`${testId}-1`}
+                  />
+                </span>
+              )}
+            </Row>
+            {rightLabel === undefined ? null : (
+              <B_15_ORANGE_463
+                onClick={rightLabel.onClick}
+                style={{ ...rightLabelPointer, ...rightLabel.style }}
+              >
+                {rightLabel.label}
+              </B_15_ORANGE_463>
             )}
           </div>
         )}
