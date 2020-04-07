95fa4b6af4b2fb86471fe48e9f34c2d979092738
Vahid Davoudi <vahid@aleph-labs.com>
2020-04-07 14:31:18 +0800

    dropdown placeholder is added

--- src/containers/Sprint_3/Sprint_3.tsx
+++ src/containers/Sprint_3/Sprint_3.tsx
@@ -90,7 +90,7 @@ const Sprint3: React.FC<Props> = () => {
   const [sTileNum3, setSTileNum3] = useState(1);
   const [stickyFooter, setStickyFooter] = useState(false);
   const [showDropdown, setDropdown] = useState(false);
-  const [dropdownValue, setDropdownValue] = useState('anything');
+  const [dropdownValue, setDropdownValue] = useState('');
   const [tacClear, setTacClear] = useState(false);
   const [profileInputValue, setProfileInputValue] = useState('');
   const [buttonColor, setButtonColor] = useState({
@@ -661,6 +661,7 @@ const Sprint3: React.FC<Props> = () => {
           icon={{ name: 'Account-2' }}
           value={dropdownValue}
           handleChange={(item) => handlerDropdown(item)}
+          placeholder = "place holder"
         />
       </div>
 
