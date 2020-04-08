4581e47b51c69250937c557a84a6830044e3175b
tim@aleph-labs.com <tim@aleph-labs.com>
2020-04-08 10:26:36 +0800

    UPDATE: Confirm Notes

--- src/components/confirmNotes/ConfirmNotes.tsx
+++ src/components/confirmNotes/ConfirmNotes.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Paragraphs from '../assets/typography';
 import classes from './ConfirmNotes.module.css';
-const { B_15_BLACK, R_15_BLACK } = Paragraphs;
+const { B_15_BLACK, R_15_BLACK, B_15_GREY969, R_15_GREY444 } = Paragraphs;
 
 interface Props {
+  testId?: string;
+  disabled?: boolean;
   title?: {
     style?: any;
     content?: string;
@@ -11,13 +13,17 @@ interface Props {
   body?: { content?: string; style?: any }[];
 }
 
-const ConfirmNotes: React.FC<Props> = ({ title, body }) => {
+const ConfirmNotes: React.FC<Props> = ({ title, body, testId, disabled }) => {
   const { style, content } = title;
   return (
-    <div className={classes.Container}>
+    <div className={classes.Container} id={testId}>
       {content && (
         <div className={classes.Title}>
-          <B_15_BLACK style={style}>{content}</B_15_BLACK>
+          {disabled ? (
+            <B_15_GREY969></B_15_GREY969>
+          ) : (
+            <B_15_BLACK style={style}>{content}</B_15_BLACK>
+          )}
         </div>
       )}
       <div className={classes.Subtitle}>
@@ -26,9 +32,13 @@ const ConfirmNotes: React.FC<Props> = ({ title, body }) => {
             const { content, style } = c;
             return (
               <div key={index} className={classes.Content}>
-                <R_15_BLACK style={style}>{`${
-                  index + 1
-                }. ${content}`}</R_15_BLACK>
+                {disabled ? (
+                  <R_15_GREY444>{`${index + 1}. ${content}`}</R_15_GREY444>
+                ) : (
+                  <R_15_BLACK style={style}>{`${
+                    index + 1
+                  }. ${content}`}</R_15_BLACK>
+                )}
               </div>
             );
           })}
