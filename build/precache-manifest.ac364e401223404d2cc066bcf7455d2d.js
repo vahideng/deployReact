154cc4d7b730a69b2c3c482755f4e2d2dc21d06f
Vahid Davoudi <vahiddavoudi@MacBook-Pro.local>
2020-05-29 13:07:01 +0800

    github deploy

---
+++ build/precache-manifest.ac364e401223404d2cc066bcf7455d2d.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bac1d4fbae776eb7e37d85db5e286ee7",
+    "url": "/testAmbank/index.html"
+  },
+  {
+    "revision": "3c2bc3c33159c6c81db2",
+    "url": "/testAmbank/static/css/2.5030c15d.chunk.css"
+  },
+  {
+    "revision": "6cf4bfc97843378326f2",
+    "url": "/testAmbank/static/css/main.60d8660f.chunk.css"
+  },
+  {
+    "revision": "3c2bc3c33159c6c81db2",
+    "url": "/testAmbank/static/js/2.c214fb70.chunk.js"
+  },
+  {
+    "revision": "2218290da18aedb408611b5cf237a0ca",
+    "url": "/testAmbank/static/js/2.c214fb70.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6cf4bfc97843378326f2",
+    "url": "/testAmbank/static/js/main.22e4b2ab.chunk.js"
+  },
+  {
+    "revision": "84b1541dd5deb476b6e9",
+    "url": "/testAmbank/static/js/runtime-main.ab793848.js"
+  },
+  {
+    "revision": "67256f81178bc896488c78d7bfda8fc4",
+    "url": "/testAmbank/static/media/ATM.67256f81.png"
+  },
+  {
+    "revision": "deb4886904e440be1463d91467bc9e8c",
+    "url": "/testAmbank/static/media/Google-Chrome.deb48869.png"
+  },
+  {
+    "revision": "c78d29f7dbb5b38f5680e54997e81c7e",
+    "url": "/testAmbank/static/media/MasterCardFooter.c78d29f7.png"
+  },
+  {
+    "revision": "ceea869cade72687c766d2abfcb3239f",
+    "url": "/testAmbank/static/media/Microsoft-Edge.ceea869c.png"
+  },
+  {
+    "revision": "39fcdd00dc9ef23dd672d8a68f15764c",
+    "url": "/testAmbank/static/media/PoliceAlert.39fcdd00.png"
+  },
+  {
+    "revision": "08c50c65167e995603e445dfbaec64ae",
+    "url": "/testAmbank/static/media/SMEInfo.08c50c65.png"
+  },
+  {
+    "revision": "7202f5769269556426b2e954c503fca1",
+    "url": "/testAmbank/static/media/Safari.7202f576.png"
+  },
+  {
+    "revision": "62b285c4157904f3c8b4896d101c2039",
+    "url": "/testAmbank/static/media/SampleQRCode.62b285c4.png"
+  },
+  {
+    "revision": "142e1d8c3a2c9b1708dc029310c3010f",
+    "url": "/testAmbank/static/media/Verisign.142e1d8c.png"
+  },
+  {
+    "revision": "22e35fda5ba9f014dc537a9424c3fc46",
+    "url": "/testAmbank/static/media/amyIcon.22e35fda.png"
+  },
+  {
+    "revision": "0f4af9dcb219fe4117cc8c2982ecb8d8",
+    "url": "/testAmbank/static/media/dotsfont.0f4af9dc.woff"
+  },
+  {
+    "revision": "13d522c14eb2752f058e1e3f992430d3",
+    "url": "/testAmbank/static/media/dotsfont.13d522c1.eot"
+  },
+  {
+    "revision": "451ebc2c6b6b13cb186652e3bfa94b91",
+    "url": "/testAmbank/static/media/dotsfont.451ebc2c.svg"
+  },
+  {
+    "revision": "e85a11a9110b2b89aa8406d888e5e252",
+    "url": "/testAmbank/static/media/dotsfont.e85a11a9.ttf"
+  },
+  {
+    "revision": "16753c520599e41a12a0a1359cd3890d",
+    "url": "/testAmbank/static/media/downloadOnAppStore.16753c52.png"
+  },
+  {
+    "revision": "36d2370c9e247ac50c22c3962cf65c42",
+    "url": "/testAmbank/static/media/downloadOnGPlay.36d2370c.png"
+  },
+  {
+    "revision": "52c485b1d1687c3c0645de385ee14407",
+    "url": "/testAmbank/static/media/passwordSecurity.52c485b1.png"
+  },
+  {
+    "revision": "233131ef9337ab2c64c1c624dd22d5ec",
+    "url": "/testAmbank/static/media/popup_curve.233131ef.png"
+  },
+  {
+    "revision": "aca8548ca53d95dd16c1f0b4344c3128",
+    "url": "/testAmbank/static/media/profilePic.aca8548c.png"
+  },
+  {
+    "revision": "38cf459a062d9693baf4d9aa6fdbb11a",
+    "url": "/testAmbank/static/media/redirect-bg.38cf459a.png"
+  },
+  {
+    "revision": "d5439a89bd01edfc087e1f76251f9d21",
+    "url": "/testAmbank/static/media/sad-amy.d5439a89.png"
+  },
+  {
+    "revision": "ee12471ae3e4cc7b9faf81da042d93c5",
+    "url": "/testAmbank/static/media/secure-image.ee12471a.png"
+  },
+  {
+    "revision": "b63a09ca10a3730ff895af1a5b83c8b0",
+    "url": "/testAmbank/static/media/slider.b63a09ca.png"
+  }
+]);
